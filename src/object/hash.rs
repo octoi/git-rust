8acf394714143b33d2f41d82c3e353a9aaa7a09d
Fadhil <fadhilsaheer@gmail.com>
2023-03-03 23:12:58 +0530

    fix: type

--- src/object/hash.rs
+++ src/object/hash.rs
@@ -1,6 +1,6 @@
 use anyhow::{anyhow, Result};
 
-pub fn validate_sha1<T: AsRef<u8>>(hash: T) -> Result<()> {
+pub fn validate_sha1<T: AsRef<[u8]>>(hash: T) -> Result<()> {
     match hex::decode(hash) {
         Ok(_) => Ok(()),
         Err(e) => Err(anyhow!("Invalid sha1 - {}", e)),
